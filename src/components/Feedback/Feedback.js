42ae5ca2d11904631fb51660f4d211bfe9687c7c
Volodymyr <mospan.volodymyr@gmail.com>
2023-02-10 13:59:12 +0200

    adds button logic

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -2,19 +2,46 @@ import React, { Component } from 'react';
 
 export default class Feedback extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    good: 1,
+    neutral: 2,
+    bad: 30,
+  };
+
+  addFeedbackStatus = e => {
+    const targetName = e.target.name;
+    console.log(this.state[targetName]);
+    //   this.setState(() => {
+    //       this.state[targetName];
+    //   })
   };
 
   render() {
     return (
       <>
         <b>Please leave feedback</b>
+
         <div>
-          <button>Good</button>
-          <button>Neutral</button>
-          <button>Bad</button>
+          <button
+            type="button"
+            name="good"
+            onClick={e => this.addFeedbackStatus(e)}
+          >
+            Good
+          </button>
+          <button
+            type="button"
+            name="neutral"
+            onClick={e => this.addFeedbackStatus(e)}
+          >
+            Neutral
+          </button>
+          <button
+            type="button"
+            name="bad"
+            onClick={e => this.addFeedbackStatus(e)}
+          >
+            Bad
+          </button>
         </div>
 
         <h2>Statistics</h2>
